4a74e933a755a46dbfd2905cd6c4bbb72d09b4c8
kabalce <k.p.balcer@gmail.com>
2022-08-17 12:37:21 +0200

    clean up

--- code_dense_hmm/run_experiments-12.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bashrun_experiments-4-8.sh
-
-python3 gaussian_test_all.py 100 40 12
-python3 gaussian_test_all.py 100 400 12
-python3 gaussian_test_all.py 100 4000 12
--- code_dense_hmm/run_experiments-20.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bashrun_experiments-4-8.sh
-
-python3 gaussian_test_all.py 100 40 20
-python3 gaussian_test_all.py 100 400 20
-python3 gaussian_test_all.py 100 4000 20
