73d9614e4d411bc127cd20d5356b00bd43a1bcab
kabalce <k.p.balcer@gmail.com>
2022-08-24 09:23:43 +0200

    disrupt hmmlearn

--- code_dense_hmm/eval_disrupted.py
+++ code_dense_hmm/eval_disrupted.py
@@ -164,88 +164,88 @@ def run_experiment(dsize, disruption, param, simple_model=True, l_fixed=True):
     # HMMlearn
     best_result["HMMlearn"] = list()
     wandb_params["init"].update({"job_type": f"n={n}-s={s}-T={s}-simple={simple_model}",
-                                 "name": f"HMMlearn"})
+                                 f"HMMlearn-dis{disruption}-p{param}-l={params['l_param']}-lr={params['cooc_lr_param']}-epochs={params['cooc_epochs_param']}"})
     wandb_params["config"].update(dict(model="HMMlearn", m=0, l=0, lr=0,
                                        em_iter=em_iter(n), cooc_epochs=0,
                                        epochs=0), scheduler=False, simple_model=simple_model)
 
     Y_disc = (Y_true > nodes.reshape(1, -1)).sum(axis=-1).reshape(-1, 1)
 
-    # for _ in range(no_rep):
-    #     hmm_monitor = HMMLoggingMonitor(tol=TOLERANCE, n_iter=0, verbose=True,
-    #                                     wandb_log=True, wandb_params=wandb_params, true_vals=true_values,
-    #                                     log_config={'metrics_after_convergence': True})
-    #     hmm_model = hmm.GaussianHMM(n, n_iter=em_iter(n))
-    #     hmm_model.monitor_ = hmm_monitor
-    #     hmm_model.fit(Y_true, lengths)
-    #
-    #     preds = hmm_model.predict(Y_true, lengths)
-    #     perm = find_permutation(preds, X_true)
-    #
-    #     best_result["HMMlearn"].append(
-    #         {
-    #             "time": time.perf_counter() - hmm_monitor._init_time,
-    #             "logprob": hmm_model.score(Y_true, lengths),
-    #             "acc": (X_true == np.array([perm[i] for i in preds])).mean(),
-    #             "dtv_transmat": dtv(hmm_model.transmat_, A[perm, :][:, perm]),
-    #             "dtv_startprob": dtv(hmm_model.startprob_, pi[perm]),
-    #             "MAE_means": (abs(mu[perm] - hmm_model.means_[:, 0])).mean(),
-    #             "MAE_sigma": (abs(sigma.reshape(-1)[perm] - hmm_model.covars_.reshape(-1))).mean(),
-    #             "dtv_omega": dtv(empirical_cooc_prob(Y_disc, n+2, lengths),
-    #                              normal_cooc_prob(hmm_model.means_.reshape(-1), hmm_model.covars_.reshape(-1), nodes[1:], A))
-    #         }
-    #     )
+    for _ in range(no_rep):
+        hmm_monitor = HMMLoggingMonitor(tol=TOLERANCE, n_iter=0, verbose=True,
+                                        wandb_log=True, wandb_params=wandb_params, true_vals=true_values,
+                                        log_config={'metrics_after_convergence': True})
+        hmm_model = hmm.GaussianHMM(n, n_iter=em_iter(n))
+        hmm_model.monitor_ = hmm_monitor
+        hmm_model.fit(Y_true, lengths)
 
-    # Custom models
-    model = models[name]
-    monitor = monitors[name]
-    alg = algs[name]
-    params = dict(l_param=int(l_param), cooc_lr_param=lr_param, cooc_epochs_param=epochs_param)
-    best_result[name] = list()
-    wandb_params["init"].update({"job_type": f"n={n}-s={s}-T={s}-simple={simple_model}",
-                                 "name": f"dis{disruption}-p{param}-l={params['l_param']}-lr={params['cooc_lr_param']}-epochs={params['cooc_epochs_param']}"})
-    wandb_params["config"].update(
-        dict(model="dense_cooc", m=0, l=int(params['l_param']), lr=params['cooc_lr_param'],
-             em_iter=em_iter(n), cooc_epochs=params['cooc_epochs_param'],
-             epochs=params['cooc_epochs_param']), scheduler=True,
-        simple_model=simple_model)
-
-    for _ in tqdm.tqdm(range(no_rep), desc=f"Training {name}"):
-        hmm_monitor = monitor(tol=TOLERANCE, n_iter=0, verbose=True,
-                              wandb_log=True, wandb_params=wandb_params, true_vals=true_values,
-                              log_config={'metrics_after_convergence': True})
-        # kmeans = KMeans(n_clusters=n, random_state=0).fit(Y_true)
-        densehmm = model(n, mstep_config={'cooc_epochs': params['cooc_epochs_param'],
-                                          'cooc_lr': params['cooc_lr_param'],
-                                          "l_uz": int(params['l_param']),
-                                          'loss_type': 'square',
-                                          'scheduler': em_scheduler},
-                         covariance_type='diag', logging_monitor=hmm_monitor, nodes=nodes,
-                         init_params="", params="stmc", early_stopping=False, opt_schemes={"cooc"},
-                         discrete_observables=m)
-        if alg == "cooc":
-            densehmm.fit_coocs(Y_true, lengths)
-        else:
-            densehmm.fit(Y_true, lengths)
-
-        preds = densehmm.predict(Y_true, lengths)
+        preds = hmm_model.predict(Y_true, lengths)
         perm = find_permutation(preds, X_true)
 
-        best_result[name].append(
+        best_result["HMMlearn"].append(
             {
                 "time": time.perf_counter() - hmm_monitor._init_time,
-                "logprob": densehmm.score(Y_true, lengths),
+                "logprob": hmm_model.score(Y_true, lengths),
                 "acc": (X_true == np.array([perm[i] for i in preds])).mean(),
-                "dtv_transmat": dtv(densehmm.transmat_, A[perm, :][:, perm]),
-                "dtv_startprob": dtv(densehmm.startprob_, pi[perm]),
-                "MAE_means": (abs(mu[perm] - densehmm.means_[:, 0])).mean(),
-                "MAE_sigma": (abs(sigma.reshape(-1)[perm] - densehmm.covars_.reshape(-1))).mean(),
+                "dtv_transmat": dtv(hmm_model.transmat_, A[perm, :][:, perm]),
+                "dtv_startprob": dtv(hmm_model.startprob_, pi[perm]),
+                "MAE_means": (abs(mu[perm] - hmm_model.means_[:, 0])).mean(),
+                "MAE_sigma": (abs(sigma.reshape(-1)[perm] - hmm_model.covars_.reshape(-1))).mean(),
                 "dtv_omega": dtv(empirical_cooc_prob(Y_disc, n+2, lengths),
-                                 normal_cooc_prob(densehmm.means_.reshape(-1), densehmm.covars_.reshape(-1), nodes[1:], A))
+                                 normal_cooc_prob(hmm_model.means_.reshape(-1), hmm_model.covars_.reshape(-1), nodes[1:], A))
             }
         )
 
-    with open(f"{RESULT_DIR}/best_result_d{disruption}_p{param}_s{s}_T{T}_n{n}_simple_model{simple_model}_l{l_fixed}.json", "w") as f:
+    # # Custom models
+    # model = models[name]
+    # monitor = monitors[name]
+    # alg = algs[name]
+    # params = dict(l_param=int(l_param), cooc_lr_param=lr_param, cooc_epochs_param=epochs_param)
+    # best_result[name] = list()
+    # wandb_params["init"].update({"job_type": f"n={n}-s={s}-T={s}-simple={simple_model}",
+    #                              "name": f"dis{disruption}-p{param}-l={params['l_param']}-lr={params['cooc_lr_param']}-epochs={params['cooc_epochs_param']}"})
+    # wandb_params["config"].update(
+    #     dict(model="dense_cooc", m=0, l=int(params['l_param']), lr=params['cooc_lr_param'],
+    #          em_iter=em_iter(n), cooc_epochs=params['cooc_epochs_param'],
+    #          epochs=params['cooc_epochs_param']), scheduler=True,
+    #     simple_model=simple_model)
+    #
+    # for _ in tqdm.tqdm(range(no_rep), desc=f"Training {name}"):
+    #     hmm_monitor = monitor(tol=TOLERANCE, n_iter=0, verbose=True,
+    #                           wandb_log=True, wandb_params=wandb_params, true_vals=true_values,
+    #                           log_config={'metrics_after_convergence': True})
+    #     # kmeans = KMeans(n_clusters=n, random_state=0).fit(Y_true)
+    #     densehmm = model(n, mstep_config={'cooc_epochs': params['cooc_epochs_param'],
+    #                                       'cooc_lr': params['cooc_lr_param'],
+    #                                       "l_uz": int(params['l_param']),
+    #                                       'loss_type': 'square',
+    #                                       'scheduler': em_scheduler},
+    #                      covariance_type='diag', logging_monitor=hmm_monitor, nodes=nodes,
+    #                      init_params="", params="stmc", early_stopping=False, opt_schemes={"cooc"},
+    #                      discrete_observables=m)
+    #     if alg == "cooc":
+    #         densehmm.fit_coocs(Y_true, lengths)
+    #     else:
+    #         densehmm.fit(Y_true, lengths)
+    #
+    #     preds = densehmm.predict(Y_true, lengths)
+    #     perm = find_permutation(preds, X_true)
+    #
+    #     best_result[name].append(
+    #         {
+    #             "time": time.perf_counter() - hmm_monitor._init_time,
+    #             "logprob": densehmm.score(Y_true, lengths),
+    #             "acc": (X_true == np.array([perm[i] for i in preds])).mean(),
+    #             "dtv_transmat": dtv(densehmm.transmat_, A[perm, :][:, perm]),
+    #             "dtv_startprob": dtv(densehmm.startprob_, pi[perm]),
+    #             "MAE_means": (abs(mu[perm] - densehmm.means_[:, 0])).mean(),
+    #             "MAE_sigma": (abs(sigma.reshape(-1)[perm] - densehmm.covars_.reshape(-1))).mean(),
+    #             "dtv_omega": dtv(empirical_cooc_prob(Y_disc, n+2, lengths),
+    #                              normal_cooc_prob(densehmm.means_.reshape(-1), densehmm.covars_.reshape(-1), nodes[1:], A))
+    #         }
+    #     )
+
+    with open(f"{RESULT_DIR}/best_result_hmmlearn_d{disruption}_p{param}_s{s}_T{T}_n{n}_simple_model{simple_model}_l{l_fixed}.json", "w") as f:
         json.dump(best_result, f, indent=4)
     return 0
 
