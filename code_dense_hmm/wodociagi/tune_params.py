95375ee578f7f7c88dc8342978ad538d64d5e422
kabalce <k.p.balcer@gmail.com>
2022-09-08 16:51:43 +0200

    debug

--- code_dense_hmm/wodociagi/tune_params.py
+++ code_dense_hmm/wodociagi/tune_params.py
@@ -1,8 +1,9 @@
 from hmmlearn import hmm
 import numpy as np
 import pandas as pd
-import os
-os.chdir("..")
+import sys
+from pathlib import Path
+sys.path.insert(0, Path(__file__).parent.absolute)
 from models_gaussian_2d import GaussianDenseHMM, HMMLoggingMonitor, DenseHMMLoggingMonitor
 import joblib
 import json
