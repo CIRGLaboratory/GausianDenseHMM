eadbb27e1f51850d68762bc64224b1f78399ad49
kabalce <k.p.balcer@gmail.com>
2022-09-22 10:06:09 +0200

    debug and rewrite data generation

--- code_dense_hmm/movielens/generate_saturation_data.py
+++ code_dense_hmm/movielens/generate_saturation_data.py
@@ -2,22 +2,41 @@ import pandas as pd
 import numpy as np
 import json
 import time
+import itertools
 from funk_svd import SVD as FSVD
 from pathlib import Path
 from tqdm import tqdm
 from pathos.multiprocessing import ProcessingPool as Pool
 
+# SETUP
 GENRE1 = "Action"
 GENRE2 = "Romance"
 
 lr = 0.001942951202698156
 reg = 0.051518838687760575
 n_epochs = 404
-# n_epochs = 2
 n_factors = 90
 
 t = time.localtime()
 RESULT_DIR = f'../../data/benchmark_rs/saturation-{t.tm_year}-{t.tm_mon}-{t.tm_mday}'
+
+np.random.seed(2022)
+no_cores = 10
+
+
+def select_users(ratings):
+    user_list = np.random.choice(ratings.u_id.unique(), 16, replace=False)
+    with open(f"{RESULT_DIR}/users.json", "w") as f:
+        json.dump(user_list.tolist(), f)
+    return user_list
+
+
+def provide_data():
+    ratings = pd.read_csv('../../data/rating.csv').rename(columns={"userId": 'u_id', "movieId": "i_id"})
+    movies = pd.read_csv('../../data/movie.csv').rename(columns={"movieId": "i_id"})
+    genres = pd.DataFrame({k: {g: True for g in v} for k, v in
+                           movies.set_index('i_id').genres.apply(lambda gs: gs.split("|")).to_dict().items()}).fillna(False).transpose()
+    return ratings, movies, genres
 
 
 def get_rating(df, u, i):
@@ -28,21 +47,47 @@ def get_rating(df, u, i):
         return tmp.values[0]
 
 
-def generate_saturation(ratings, movies_available,  step, new_scores_tmp):
-    ratings_tmp = pd.concat([ratings, new_scores_tmp], axis=0)
-    movies_available_tmp = movies_available.copy()
-    movies_available_tmp = movies_available_tmp.set_index(['u_id', 'i_id']).combine_first(new_scores_tmp.set_index(['u_id', 'i_id'])).reset_index()
-
+def provide_ratings(train, test):
     fsvd = FSVD(lr=lr, reg=reg, n_epochs=n_epochs, n_factors=n_factors,
                 early_stopping=True, shuffle=False, min_rating=1, max_rating=5)
-    fsvd.fit(ratings_tmp)
-    preds = fsvd.predict(movies_available_tmp)
+    fsvd.fit(train)
+    preds = fsvd.predict(test)
+    return preds
 
-    # calculate the saturation
-    res_tmp = pd.concat([movies_available_tmp, pd.DataFrame({"pred": preds})], axis=1)
-    res_tmp.to_parquet(f"{RESULT_DIR}/predictions_step_{step}.parquet")
 
-    # print(res_tmp)
+def provide_all_available(scores, user):
+    items = scores.i_id.drop_duplicates()
+    all = pd.merge(pd.DataFrame([(u, i) for i, u in itertools.product(items, user)], columns=["u_id", "i_id"]),
+                   ratings.drop('timestamp', axis=1),
+                   how='left', on=['u_id', 'i_id'])
+    all_available = all.loc[all.rating.isna(), :].drop('rating', axis=1)
+    return all_available
+
+
+def sample_new_scores(available, genre, sample_size):
+    available_all_genres = pd.merge(all_available,
+                                    genres.reset_index().rename(columns={'index': 'i_id'}),
+                                    how="left", on="i_id")
+    available_genres = available_all_genres.loc[available_all_genres[GENRE1] | available_all_genres[GENRE2],
+                                                ['u_id', 'i_id', 'preds']]
+    new_scores = available_genres.groupby('u_id').apply(
+        lambda df: pd.DataFrame(
+            {'i_id': np.random.choice(df.sort_values('preds').i_id.values[-100:], sample_size, replace=False),
+             'rating': np.random.choice(np.array([4, 5]), sample_size, p=np.array([.15, .85]))})
+    ).reset_index().drop('level_1', axis=1)
+    return new_scores
+
+
+def generate_saturation(args):
+    rats, new_s, all_avail, step, iter = args
+    new_scores_tmp = new_s.groupby('u_id').apply(lambda df: df.iloc[:step, :])
+    ratings_tmp = pd.concat([rats, new_scores_tmp])
+    all_available_tmp = all_avail.loc[~(all_avail.u_id.isin(new_scores_tmp.u_id) & all_avail.i_id.isin(new_scores_tmp.i_id)), :]
+
+    preds = provide_ratings(ratings_tmp, all_available_tmp)
+
+    res_tmp = pd.concat([all_available_tmp, pd.DataFrame({"pred": preds})], axis=1)
+    res_tmp.to_parquet(f"{RESULT_DIR}/predictions_step_{iter * no_cores + step}.parquet")
 
     saturation = res_tmp.groupby("u_id").apply(
         lambda df: df.sort_values("pred")[-100:].drop(["u_id", "i_id", "pred"], axis=1).mean()).to_dict("index")
@@ -50,66 +95,24 @@ def generate_saturation(ratings, movies_available,  step, new_scores_tmp):
     return saturation
 
 
-# if __name__ == "__main__":
-print("START")
+if __name__ == "__main__":
+    Path(RESULT_DIR).mkdir(exist_ok=True, parents=True)
+    ratings, movies, genres = provide_data()
+    users = select_users(ratings)
 
-Path(RESULT_DIR).mkdir(exist_ok=True, parents=True)
-ratings = pd.read_csv('../../data/rating.csv').rename(columns={"userId": 'u_id',  "movieId": "i_id"})
-movies = pd.read_csv('../../data/movie.csv').rename(columns={"movieId": "i_id"})
-genres = pd.DataFrame({k: {g: True for g in v} for k, v in movies.set_index('i_id').genres.apply(lambda gs: gs.split("|")).to_dict().items()}).fillna(False).transpose()
+    saturation_list = []
 
-print("Read data - DONE")
+    for i in range(100):
+        # Provide new scores
+        all_available = provide_all_available(ratings, users)
+        all_available['preds'] = provide_ratings(ratings, all_available)
+        new_scores = sample_new_scores(all_available, genres, no_cores)
 
-np.random.seed(2022)
+        with Pool(nodes=no_cores) as pool:
+            saturation_list += pool.map(generate_saturation,
+                                        itertools.product([ratings], [new_scores], [all_available], range(no_cores), [i]))
 
-users = np.random.choice(ratings.u_id.unique(), 16, replace=False)
-with open(f"{RESULT_DIR}/users.json", "w") as f:
-    json.dump(users.tolist(), f)
+        ratings = pd.concat([ratings, new_scores])
 
-all_movies = genres.index.values
-
-movies_tmp = ratings.loc[ratings.u_id.isin(users)].pivot('u_id', 'i_id', 'rating')
-movies_available = pd.melt(movies_tmp.reset_index(),
-                           id_vars='u_id',
-                           value_vars=movies_tmp.columns,
-                           var_name='i_id', value_name='rating_real')
-
-movies_available = pd.concat([movies_available, genres.loc[movies_available.i_id].reset_index(drop=True)], axis=1)
-
-print("Prepare variables - DONE")
-
-saturation_list = []
-
-
-for i in tqdm(range(100)):
-    genre_tmp = GENRE1 if i % 2 else GENRE2
-    #  provide new scores for 25 iterations
-    fsvd = FSVD(lr=lr, reg=reg, n_epochs=n_epochs, n_factors=n_factors,
-                early_stopping=True, shuffle=False, min_rating=1, max_rating=5)
-    fsvd.fit(ratings)
-    preds = fsvd.predict(movies_available)
-
-    # calculate the saturation
-    movies_available = pd.concat([movies_available, pd.DataFrame({"pred": preds})], axis=1)
-
-    selected = movies_available.loc[movies_available.i_id.isin(genres.index[genres[genre_tmp]]), :].groupby("u_id").apply(lambda df: pd.Series(np.random.choice(df.sort_values("pred")[-100:].index, 10, False))).melt().value
-    new_scores = pd.concat([movies_available.loc[selected, ["u_id", "i_id"]].reset_index(drop=True),
-                            pd.DataFrame({"rating": np.random.choice([4, 5], selected.shape[0], p=[.15, .85])})],
-                           axis=1)
-
-    def task(j, new_scores=new_scores, i=i):
-        new_scores_tmp = new_scores.groupby("u_id").apply(lambda df: df[:(j + 1)])
-        # print(new_scores_tmp.shape)
-        saturation = generate_saturation(ratings, movies_available, i * 10 + j, new_scores_tmp)
-        return saturation
-
-
-    movies_available.drop("pred", axis=1, inplace=True)
-    with Pool(nodes=10) as pool:
-        saturation_list += pool.map(task, list(range(10)))
-
-    movies_available.loc[selected, 'rating'] = new_scores.rating.values
-    ratings = pd.concat([ratings, new_scores], axis=0)
-
-with open(f"{RESULT_DIR}/all_saturation.json", "w") as f:
-    json.dump(saturation_list, f, indent=4)
+    with open(f"{RESULT_DIR}/all_saturation.json", "w") as f:
+        json.dump(saturation_list, f, indent=4)
