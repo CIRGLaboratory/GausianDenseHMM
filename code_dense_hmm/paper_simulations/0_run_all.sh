c1b7598f3365cd927849855d6605b24887ac5c8a
Klaudia <k.p.balcer@gmail.com>
2022-11-06 17:32:03 +0100

    run all

---
+++ code_dense_hmm/paper_simulations/0_run_all.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+python3 0_baseline.py -n 2 -d 1 -T 100 &
+python3 0_baseline.py -n 3 -d 1 -T 100 &
+python3 0_baseline.py -n 5 -d 1 -T 100 &
+python3 0_baseline.py -n 7 -d 1 -T 100 &
+python3 0_baseline.py -n 10 -d 1 -T 100 &
+python3 0_baseline.py -n 20 -d 1 -T 100 &
+python3 0_baseline.py -n 2 -d 1 -T 1000 &
+python3 0_baseline.py -n 3 -d 1 -T 1000 &
+python3 0_baseline.py -n 5 -d 1 -T 1000 &
+python3 0_baseline.py -n 7 -d 1 -T 1000 &
+python3 0_baseline.py -n 10 -d 1 -T 1000 &
+python3 0_baseline.py -n 20 -d 1 -T 1000 &
+python3 0_baseline.py -n 2 -d 1 -T 10000 &
+python3 0_baseline.py -n 3 -d 1 -T 10000 &
+python3 0_baseline.py -n 5 -d 1 -T 10000 &
+python3 0_baseline.py -n 7 -d 1 -T 10000 &
+python3 0_baseline.py -n 10 -d 1 -T 10000 &
+python3 0_baseline.py -n 20 -d 1 -T 10000 &
+python3 0_baseline.py -n 2 -d 1 -T 100000 &
+python3 0_baseline.py -n 3 -d 1 -T 100000 &
+python3 0_baseline.py -n 5 -d 1 -T 100000 &
+python3 0_baseline.py -n 7 -d 1 -T 100000 &
+python3 0_baseline.py -n 10 -d 1 -T 100000 &
+python3 0_baseline.py -n 20 -d 1 -T 100000
+python3 0_baseline.py -n 20 -d 1 -T 100000
