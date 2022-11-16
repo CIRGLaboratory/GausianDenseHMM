288890a9c075c4acb9afc27c84fa33cff6f15841
Klaudia <k.p.balcer@gmail.com>
2022-11-16 13:50:13 +0100

    try to debug

--- code_dense_hmm/paper_simulations/2_dense_reproducibility.py
+++ code_dense_hmm/paper_simulations/2_dense_reproducibility.py
@@ -10,7 +10,7 @@ from scipy.special import erf
 from sklearn.cluster import KMeans
 from sklearn.tree import DecisionTreeClassifier
 from ssm.util import find_permutation
-
+import tensorflow as tf
 np.random.seed(2137)
 
 # Parse arguments:
@@ -191,7 +191,7 @@ def eval_dense_model(n_, Y_train, X_train, lengths_, d_):
     ll = model.score(Y_train, lengths_)
     states = model.predict(Y_train, lengths_)
     acc = (find_permutation(states, X_train)[states] == X_train).mean()
-    loss = compute_loss(nodes, splits, n_, omega_gt, model.means_, model.covars_, model.transmat_), model.loss_cooc
+    loss = compute_loss(nodes, splits, n_, omega_gt, model.means_, model.covars_, model.transmat_), tf.get_static_value(model.loss_cooc)
     return ll, loss, acc, end - start
 
 
