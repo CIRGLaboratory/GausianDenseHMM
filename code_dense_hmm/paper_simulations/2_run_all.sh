0dff84c528f5d3120c3d2c3b9df1b07f80bf6e12
Klaudia <k.p.balcer@gmail.com>
2022-11-25 11:25:15 +0100

    add  big models

--- code_dense_hmm/paper_simulations/2_run_all.sh
+++ code_dense_hmm/paper_simulations/2_run_all.sh
@@ -14,5 +14,8 @@ python3 2_dense_reproducibility.py -n 7 -d 2 -T 10000
 python3 2_dense_reproducibility.py -n 10 -d 1 -T 1000000
 python3 2_dense_reproducibility.py -n 10 -d 2 -T 1000000
 
-# python3 2_dense_reproducibility.py -n 20 -d 1 -T 10000000
-# python3 2_dense_reproducibility.py -n 20 -d 2 -T 10000000
+python3 2_dense_reproducibility.py -n 20 -d 1 -T 10000000
+python3 2_dense_reproducibility.py -n 20 -d 2 -T 10000000
+
+python3 2_dense_reproducibility.py -n 100 -d 1 -T 100000000
+python3 2_dense_reproducibility.py -n 100 -d 2 -T 100000000
